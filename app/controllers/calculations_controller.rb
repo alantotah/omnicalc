d788b8c4c94e0f540109bc05a4fd86db32163030
alantotah <alantotah@users.noreply.github.com>
2017-04-16 12:16:39 -0500

    omnicalc_v1

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -10,14 +10,15 @@ class CalculationsController < ApplicationController
     # The special word the user input is in the string @special_word.
     # ================================================================================
 
+    text_split_into_array = @text.split
 
-    @word_count = "Replace this string with your answer."
+    @word_count = text_split_into_array.length
 
-    @character_count_with_spaces = "Replace this string with your answer."
+    @character_count_with_spaces = @text.size
 
-    @character_count_without_spaces = "Replace this string with your answer."
+    @character_count_without_spaces = @text.size - @text.scan(/\s+/).size
 
-    @occurrences = "Replace this string with your answer."
+    @occurrences = text_split_into_array.count(@special_word)
 
     # ================================================================================
     # Your code goes above.
@@ -38,7 +39,7 @@ class CalculationsController < ApplicationController
     # The principal value the user input is in the decimal @principal.
     # ================================================================================
 
-    @monthly_payment = "Replace this string with your answer."
+    @monthly_payment = ((((@apr/100)/12) * @principal) / (1 - (1 + ((@apr/100)/12)) ** ( -(@years * 12))))
 
     # ================================================================================
     # Your code goes above.
@@ -60,12 +61,12 @@ class CalculationsController < ApplicationController
     #   number of seconds as a result.
     # ================================================================================
 
-    @seconds = "Replace this string with your answer."
-    @minutes = "Replace this string with your answer."
-    @hours = "Replace this string with your answer."
-    @days = "Replace this string with your answer."
-    @weeks = "Replace this string with your answer."
-    @years = "Replace this string with your answer."
+    @seconds = @ending - @starting
+    @minutes = @seconds / 60
+    @hours = @minutes / 60
+    @days = @hours / 24
+    @weeks = @days / 7
+    @years = @weeks / 52
 
     # ================================================================================
     # Your code goes above.
@@ -82,25 +83,34 @@ class CalculationsController < ApplicationController
     # The numbers the user input are in the array @numbers.
     # ================================================================================
 
-    @sorted_numbers = "Replace this string with your answer."
+    sorted = @numbers.sort
+    len = sorted.length
 
-    @count = "Replace this string with your answer."
+    def variance(array)
+      arr = []
+      array.each { |i| arr << (i - @sum/@count) ** 2}
+      arr.sum/@count
+    end
 
-    @minimum = "Replace this string with your answer."
+    @sorted_numbers = sorted
 
-    @maximum = "Replace this string with your answer."
+    @count = len
 
-    @range = "Replace this string with your answer."
+    @minimum = @numbers.min
 
-    @median = "Replace this string with your answer."
+    @maximum = @numbers.max
 
-    @sum = "Replace this string with your answer."
+    @range = @maximum - @minimum
 
-    @mean = "Replace this string with your answer."
+    @median = (sorted[(len - 1) / 2] + sorted[len / 2]) / 2.0
 
-    @variance = "Replace this string with your answer."
+    @sum = @numbers.sum
 
-    @standard_deviation = "Replace this string with your answer."
+    @mean = @sum/@count
+
+    @variance = variance(@numbers)
+
+    @standard_deviation = Math.sqrt(@variance)
 
     @mode = "Replace this string with your answer."
 
